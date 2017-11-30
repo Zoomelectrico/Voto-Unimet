93be3eb9a39caf67d69cfe740471f285baac0363
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2017-11-30 14:25:59 -0400

    Nose

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,45 +1,41 @@
 // Configuracipn de Firebase
 var config = {
-  apiKey: "AIzaSyBa0nE6NxMXyBDwzG-a4VFK3AVRnl-ueHU",
-  authDomain: "votounimet.firebaseapp.com",
-  databaseURL: "https://votounimet.firebaseio.com",
-  projectId: "votounimet",
-  storageBucket: "votounimet.appspot.com",
-  messagingSenderId: "601104183423"
+  	apiKey: "AIzaSyBa0nE6NxMXyBDwzG-a4VFK3AVRnl-ueHU",
+    authDomain: "votounimet.firebaseapp.com",
+    databaseURL: "https://votounimet.firebaseio.com",
+    projectId: "votounimet",
+    storageBucket: "votounimet.appspot.com",
+    messagingSenderId: "601104183423"
 };
 firebase.initializeApp(config);
 
 //Constantes de Firebase
 const db = firebase.database();
-const auth = firebase.auth();
-
 // Inicio de Sesion
 $('#login').click(function(){
-	var user = firebase.auth().currentUser;
-	if(user){
-		logout();
-	} else {
-		login();
-	}	
+	console.log('Flag 3');
+	login();
 });
 // Funciones de Auth
 function login () {
 	var provedor =  new firebase.auth.GoogleAuthProvider();
-	auth.signInWithRedirect(provedor)
-		.then(function(result){
+	firebase.auth().signInWithRedirect(provedor).then(function(result){
+			console.log('Flag 4');
 			var token = result.credential.accessToken;
-			var user = result.user;
-			//TODO: A que chequear al usuario y registrarlo y toda la paja
-		}).catch(function(error){
+  			var user = firebase.auth().currentUser;
+  			if (typeof user === "undefined") {console.log('aqui')}
+  			db.ref('User/'+user.UID).set({
+  				UID: user.UID,
+  				nombre: user.displayName,
+  			});
+  		}).catch(function(error){
 		console.log(error);
 	});  
 }
 function logout () {
-	auth.signOut()
-		.then(function(){
-			// Beio
+	firebase.auth().signOut().then(function(){
+			console.log('Flag 5');
 		}).catch(function(error){
 			console.log(error);
 		});
 }
-
