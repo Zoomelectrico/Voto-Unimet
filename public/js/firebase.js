a7f99abaf2bd6b81af3b15a7a9d0a813da167449
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2017-12-01 15:08:10 -0400

    Ya medio sirve

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -9,39 +9,52 @@ var config = {
 };
 firebase.initializeApp(config);
 
-document.getElementById('signup').addEventListener('click', function(){
-	var email = document.getElementById('email').value;
-	var pass = document.getElementById('password').value;
-	if (email.endsWith('@correo.unimet.edu.ve')) {
-		signup(email, pass);	
-	} else {
-		alert('Usuario no permitido')
-	}
-	
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+  	console.log('1');
+  	console.log(user);
+  	$('#chip-div').removeClass('hide');
+  	$('#user-name').append(user.displayName);
+  	$('#img-user').attr("src",user.photoURL);
+  	$('#logout').removeClass('hide');
+  	$('#login').addClass('hide');
+  	document.getElementById('user-name').innerHTML = user.displayName;
+  } else {
+  	console.log('2');
+    $('#chip-div').addClass('hide');
+    $('#logout').addClass('hide');
+    $('#login').removeClass('hide');
+  }
 });
 
-document.getElementById('login').addEventListener('click', function(){
-	var email = document.getElementById('email').value;
-	var pass = document.getElementById('password').value;
-	if (email.endsWith('@correo.unimet.edu.ve')) {
-		login(email, pass);	
-	} else {
-		alert('Usuario no permitido')
-	}
-});
+document.querySelector("#login").addEventListener('click', signIn);
+document.querySelector("#login-m").addEventListener('click', signInMobile);
 
-function signup (email,pass) {
-	firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
-  		var errorCode = error.code;
-  		var errorMessage = error.message;
-  		console.log(error);
-	});
+function signIn () {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+	    var user = result.user;
+	    firebase.database().ref('User/' + user.uid).set({
+			UID: user.uid,
+			nombre: user.displayName,
+			email: user.email
+		});
+    }).catch(function(error) {});
 }
 
-function login (email,pass) {
-	firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
- 	var errorCode = error.code;
-  	var errorMessage = error.message;
-  	console.log(error)
-});
+function signInMobile () {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider).then(function(result) {
+	    var user = result.user;
+	    firebase.database().ref('User/' + user.uid).set({
+			UID: user.uid,
+			nombre: user.displayName,
+			email: user.email
+		});
+    }).catch(function(error) {});
+}
+
+document.querySelector("#logout").addEventListener('click', signOut);
+function signOut(){
+	firebase.auth().signOut().then(function () {}).catch(function (error) {});
 }
