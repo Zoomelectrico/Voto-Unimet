11b64e19b9752c5520a03f42ac509c29885f7249
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2017-12-06 15:13:20 -0400

    FALTA SUMAR LOS VOTOS

---
+++ public/assets/js/agregar-candidato/agregar-candidato.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  document.getElementById('agregar').addEventListener('click', function() {
+    var file = document.getElementById("fileUpload").files[0];
+    const storageRef = firebase.storage().ref();
+    const uploadTask = storageRef.child('Candidatos/' + file.name).put(file);
+    uploadTask.on('state_changed', function(snapshot) {
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function(error) {
+      console.log(error)
+    }, function() {
+      firebase.database().ref('Candidatos/'+document.getElementById('nombre').value+'/').set({
+        nombre: document.getElementById('nombre').value,
+        email: document.getElementById('email').value,
+        cargo: document.getElementById('cargo').value,
+        plancha: document.getElementById('plancha').value,
+        foto: uploadTask.snapshot.downloadURL
+      });
+      alert('agregado con exito');
+      window.location = 'index.html';
+    });
+  });
+});
