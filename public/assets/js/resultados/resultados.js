0f058467538851da9a772a3e51bfd61988f16706
Germano Rojas <germano.rojas@correo.unimet.edu.ve>
2017-12-06 15:31:02 -0400

    A ver

--- public/assets/js/resultados/resultados.js
+++ public/assets/js/resultados/resultados.js
@@ -1,5 +1,5 @@
 // Llenar tabla
-var ref = firebase.database().ref("Candidatos/CID");
+/*var ref = firebase.database().ref("Candidatos/CID");
 ref.once("value")
   .then(function(snapshot) {
     var name = snapshot.child("Nombre").val(); // {first:"Ada",last:"Lovelace"}
@@ -16,7 +16,49 @@ ref.once("value")
     $("#tabla").append(d);
   });
 
+ref.once('value', function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+    
+  });
+});
+*/
 
+
+   
+
+
+	var db = firebase.database();
+	var ref = db.ref("Candidatos");
+	     
+	var table = document.getElementById("tabla");
+	    
+	//limpia la tabla
+	table.innerHTML = "";
+	 
+	 //con esta función recorre todos los datos almacenados en FB ordenados por mi child(tipo)
+	    
+	ref/*.orderByChild("tipo")*/.on("child_added", function(snapshot){
+	//repite el proceso como cuantas referencias encuentre y los asigna a la lista "d"
+	     
+	  var d = snapshot.val(); 
+	  console.log(d.Nombre);       
+	        
+	  {
+	  var row = table.insertRow(0);
+	  var cell1 = row.insertCell(0);
+	  var cell2 = row.insertCell(1);
+	  var cell3 = row.insertCell(2);
+	  
+	  // asigna a las celdas el valir del Child especificado
+	  cell1.innerHTML = d.Nombre;
+	  cell2.innerHTML = d.Candidato;
+	  cell3.innerHTML = d.Nombre;
+
+	  }
+	 
+	        
+	});
+	 
 
 
 
