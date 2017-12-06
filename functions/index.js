73d59822bb51fed638e3aa04fd4a03dc8ce4ac05
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2017-12-06 11:20:54 -0400

    Añadi la función de Email

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword
+  }
+});
+
+const APP_NAME = 'Voto Unimet'; //Nombre de la aplicacion
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
+  const user = event.data; // The Firebase user.
+  const email = user.email; // The email of the user.
+  const displayName = user.displayName; // The name of the user
+  return welcomeEmail(email, displayName); //The promise return
+});
+
+function welcomeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <norespondervotounimet@gmail.com>`,
+    to: email
+  };
+
+  // The user subscribed to the newsletter.
+  mailOptions.subject = `Welcome to ${APP_NAME}!`;
+  mailOptions.text = `Hola ${displayName || ''}! Bienvenido a ${APP_NAME}. \n Esperamos que tu experiencia sea placentera, y no olvides votar. \n\n Sinceramente el Equipo de Voto Unimet`;
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('New welcome email sent to:', email);
+  });
+}
